a4709862e1bf75a365fa7546fd59d780a6af9ead
SimoneFerraris <siferraris@unibz.it>
2023-11-11 06:29:01 +0100

    edit layout chargingPage

--- src/fragments/Containers.js
+++ src/fragments/Containers.js
@@ -17,50 +17,5 @@ function InstructionsContainer(objInfo) {
       );
 };
 
-function ChargingContainer() {
-    return (
-        <div style={{justifyContent: 'center', height: '50vh' }}>
-            <div className = {`chargingContainer ${styles.chargingContainer}`}>
-                <h1 style={{paddingLeft: '50px', paddingTop: '10px', fontSize: '45px' }}>Charging...</h1>
-            </div>
-        </div>
-    )
-}
 
-function POIContainer() {
-    return (
-        <div style={{  justifyContent: 'center', height: '50vh' }}>
-            <div className = {`poiContainer ${styles.poiContainer}`}>
-                
-                <Box sx={{ flexGrow: 1, justifyContent: 'center' }}>
-                <Grid container rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                        <Grid item xs={12}>
-                            <h1 style={{paddingLeft: '50px', paddingTop: '15px', fontSize: '45px' }}>Getting bored?</h1>
-                        </Grid>
-                        <Grid item xs={6}>
-                            {buttonContainer}
-                        </Grid>
-                        <Grid item xs={6}>
-                            {buttonContainer}
-                        </Grid>
-                        <Grid item xs={6}>
-                            {buttonContainer}
-                        </Grid>
-                        <Grid item xs={6}>
-                            {buttonContainer}
-                        </Grid>
-                    </Grid>
-                </Box>
-            </div>
-        </div>
-    );
-    
-}
-
-const buttonContainer = 
-    <>
-    <div className = {`buttonContainer ${styles.buttonContainer}`}>
-    </div>
-    </>
-
-export {InstructionsContainer, ChargingContainer, POIContainer};
+export {InstructionsContainer};
--- src/pages/ChargingPage.js
+++ src/pages/ChargingPage.js
@@ -1,4 +1,3 @@
-import { POIContainer, ChargingContainer } from "../fragments/Containers";
 import gastronomy from "../images/gastronomy.svg";
 import shopping from "../images/shopping.svg";
 import help from "../images/help.svg";
@@ -47,13 +46,12 @@ function ChargingPage() {
             </div>
           </div>
           <div className="container-charging">
-            <div className="container-help">
                 <img className="img-help" alt="help" src={help} />
               <Lottie className="carbattery" animationData={carBattery} loop={true} />            
               <div className="text-h-left">00:13:00 h left</div>
               <div className="text-car-power">Power: 6.0 kWh</div>
               <div className="text-kwh">0.0121 kWh</div>
-            </div>
+            
             <div className="text-charging">Charging...</div>
           </div>
         </div>
@@ -61,56 +59,5 @@ function ChargingPage() {
     </div>
   );
 }
-//<img className="carbattery" alt="Carbattery" src="https://c.animaapp.com/dHkemGNM/img/carbattery-1.svg" />
 
 export default ChargingPage;
-
-/*
-import React from "react";
-import "./style.css";
-
-export const Box = () => {
-  return (
-    <div className="box">
-      <div className="main">
-        <div className="overlap">
-          <div className="overlap-group">
-            <div className="div">
-              <div className="eat-container" />
-              <div className="text-wrapper">Getting bored?</div>
-              <div className="text-wrapper-2">Gastronomy</div>
-              <img className="gastronomy-img" alt="Gastronomy img" src="gastronomy-img.svg" />
-            </div>
-            <div className="overlap-2">
-              <div className="text-wrapper-3">Shops</div>
-              <img className="shopping-img" alt="Shopping img" src="shopping-img.svg" />
-            </div>
-            <div className="overlap-3">
-              <div className="text-wrapper-4">Discover</div>
-              <img className="discover-img" alt="Discover img" src="discover-img.svg" />
-            </div>
-            <div className="overlap-4">
-              <div className="text-wrapper-5">Activities</div>
-              <img className="activities-img" alt="Activities img" src="activities-img.svg" />
-            </div>
-          </div>
-          <div className="overlap-5">
-            <div className="overlap-6">
-              <div className="help">
-                <div className="overlap-group-2">
-                  <img className="vector" alt="Vector" src="vector.svg" />
-                  <img className="img" alt="Vector" src="image.svg" />
-                  <div className="rectangle" />
-                </div>
-              </div>
-              <img className="carbattery" alt="Carbattery" src="carbattery-1.svg" />
-            </div>
-            <div className="text-wrapper-6">Charging...</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-*/
